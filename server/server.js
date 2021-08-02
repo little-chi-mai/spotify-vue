ed4e6ce8de9d1196f6eedc5f75497e78721bffd5
Mai Nguyen <chimai.dk@gmail.com>
2021-08-02 13:05:34 +1000

    hard coded redirect uri

--- server/server.js
+++ server/server.js
@@ -118,7 +118,7 @@ function callback(req, res) {
 
       spotifyApi.setAccessToken(data.body["access_token"]);
       spotifyApi.setRefreshToken(data.body["refresh_token"]);
-      res.redirect(process.env.ROOT_CLIENT);
+      res.redirect("https://pensive-torvalds-729fbd.netlify.app");
     })
     .catch(function (err) {
       console.log("Something went wrong:", err);
