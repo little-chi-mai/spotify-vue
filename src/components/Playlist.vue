e67ee8bc25490b9b3c8379c0cfd893e887a1048c
Mai Nguyen <chimai.dk@gmail.com>
2021-08-05 17:22:09 +1000

    change something..

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,22 +1,6 @@
 <template>
   <div v-if="name">
     <h1>Playlist <span>{{ name }}</span></h1>
-    <!-- <a :href="url" target="_blank"
-      >Click to listen to this playlist on Spotify</a
-    > -->
-    <!-- <div v-for="item in tracks.items" :key="item.track.id">
-      <p>{{ item.track.name }}</p>
-      <a :href="item.track.external_urls.spotify" target="_blank">
-        <img :src="item.track.album.images[2].url" @hover="playMusic" alt="" />
-      </a>
-      <audio controls>
-        <source :src="item.track.preview_url" type="audio/mpeg" />
-
-        Your browser does not support the audio element.
-      </audio>
-    </div> -->
-
-    <!-- <p>{{ track.track.name }}</p> -->
   </div>
 </template>
 
